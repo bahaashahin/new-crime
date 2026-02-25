6d1d49d372f949a5428209eda0e23e648051e83b
bahaashahin <bahaatarek579@gmail.com>
2026-02-25 02:15:44 +0200

    maaaa

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -34,70 +34,65 @@ function Home() {
 
   return (
     <>
-      {/* HERO SECTION */}
-      <HeroSection
-        // viVideo1}
-        background={HeroImg1}
-      />
-      {/* MUBAHITH CARDS */}
-      <section
-        dir="rtl"
-        className="py-20 px-6 min-h-screen"
-      >
-        <h2 className="text-3xl md:text-4xl  text-center mb-12 font-bold text-white z-10 px-6">
-          التحريات الجنائية في مكافحة الأموال العامة
-        </h2>
-        <QuranAyah
-          text={`بسم الله الرحمن الرحيم
+      <div className="min-h-screen text-white" dir="rtl">
+        {/* HERO SECTION */}
+        <HeroSection background={HeroImg1} />
+        {/* MUBAHITH CARDS */}
+        <section className="max-w-6xl mx-auto px-6 py-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 gap-6">
+          <h2 className="text-3xl md:text-4xl  text-center mb-12 font-bold text-white z-10 px-6">
+            التحريات الجنائية في مكافحة الأموال العامة
+          </h2>
+          <QuranAyah
+            text={`بسم الله الرحمن الرحيم
 
             ﴿ إِنَّ اللَّهَ يَأْمُرُكُمْ أَنْ تُؤَدُّوا الْأَمَانَاتِ إِلَىٰ أَهْلِهَا وَإِذَا حَكَمْتُمْ بَيْنَ النَّاسِ أَنْ تَحْكُمُوا بِالْعَدْلِ ﴾`}
-          speed={45}
-          className="mt-20 mb-20 max-w-5xl mx-auto px-6"
-        />
-        <ModalVideo src={Video1} label="تشغيل الفيديو" />
+            speed={45}
+            className="mt-20 mb-20 max-w-5xl mx-auto px-6"
+          />
+          <ModalVideo src={Video1} label="تشغيل الفيديو" />
 
-        
-        <ScrollReveal direction="right" delay={0.2}>
-          <h3 className="text-3xl md:text-4xl font-bold text-center mb-12">
-            مباحث جرائم الأموال العامة
-          </h3>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
-            {mubahith.map((item, index) => (
-              <Link
-                to={item.path}
-                key={index}
-                className="group relative rounded-2xl p-[1px] 
+          {/* <ScrollReveal direction="right" delay={0.2}> */}
+            <h3 className="text-3xl md:text-4xl font-bold text-center mb-12">
+              مباحث جرائم الأموال العامة
+            </h3>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+              {mubahith.map((item, index) => (
+                <Link
+                  to={item.path}
+                  key={index}
+                  className="group relative rounded-2xl p-[1px] 
                          bg-gradient-to-br from-[#0ea5e9] to-[#1e3a8a]
                          transition-all duration-500 hover:scale-105"
-              >
-                <div className="relative h-full rounded-2xl bg-[#020617] p-6 flex flex-col justify-between overflow-hidden">
-                  {/* Glow Effect */}
-                  <div
-                    className="absolute inset-0 opacity-0 group-hover:opacity-20 transition duration-500
+                >
+                  <div className="relative h-full rounded-2xl bg-[#020617] p-6 flex flex-col justify-between overflow-hidden">
+                    {/* Glow Effect */}
+                    <div
+                      className="absolute inset-0 opacity-0 group-hover:opacity-20 transition duration-500
                                 bg-gradient-to-br from-sky-500 to-blue-800"
-                  />
+                    />
 
-                  <div>
-                    <h3 className="text-xl font-bold mb-3 group-hover:text-sky-400 transition">
-                      {item.title}
-                    </h3>
-                    <p className="text-sm text-gray-300 leading-relaxed">
-                      {item.desc}
-                    </p>
-                  </div>
+                    <div>
+                      <h3 className="text-xl font-bold mb-3 group-hover:text-sky-400 transition">
+                        {item.title}
+                      </h3>
+                      <p className="text-sm text-gray-300 leading-relaxed">
+                        {item.desc}
+                      </p>
+                    </div>
 
-                  <span
-                    className="mt-6 inline-block text-sm font-semibold text-sky-400
+                    <span
+                      className="mt-6 inline-block text-sm font-semibold text-sky-400
                                  group-hover:-translate-x-2 transition"
-                  >
-                    الدخول إلى المبحث ←
-                  </span>
-                </div>
-              </Link>
-            ))}
-          </div>
-        </ScrollReveal>
-      </section>
+                    >
+                      الدخول إلى المبحث ←
+                    </span>
+                  </div>
+                </Link>
+              ))}
+            </div>
+          {/* </ScrollReveal> */}
+        </section>
+      </div>
     </>
   );
 }
